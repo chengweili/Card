7dd9c8e49f9e20a33fab2ddfd1332526605a8d8a
chengweili <chengweili42@gmail.com>
2016-01-20 08:28:36 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,42 +1,22 @@
-
+import java.awt.Frame;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class GUI implements ActionListener {
-    private int clicks = 0;
-    private JLabel label = new JLabel("Number of clicks:  0     ");
-    private JFrame frame = new JFrame();
+public class GUI(){
 
-    public GUI() {
+    public GUI(){
 
-        // the clickable button
-        JButton button = new JButton("Click Me");
-        button.addActionListener(this);
-
-        // the panel with the button and text
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0, 1));
-        panel.add(button);
-        panel.add(label);
-
-        // set up the frame and display it
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("GUI");
-        frame.pack();
-        frame.setVisible(true);
     }
 
-    // process the button clicks
-    public void actionPerformed(ActionEvent e) {
-        clicks++;
-        label.setText("Number of clicks:  " + clicks);
-    };
+    public Label(String strLabel, int alignment); // Construct a Label with the given text String, of the text alignment
+    public Label(String strLabel);                // Construct a Label with the given text String
+    public Label();                               // Construct an initially empty Label
 
-    // create one Frame
-    public static void main(String[] args) {
+    
+
+    public static void main(String [] args){
         new GUI();
     }
+
 }
