a384b9ab51fa713aaa4b293634b8d3437ce85804
chengweili <chengweili42@gmail.com>
2016-01-22 08:38:14 -0500

    Update Card.java

--- Card.java
+++ Card.java
@@ -2,10 +2,13 @@ import wheels.users.*;
 
 public class Card extends Rectangle{
 
+    private Rectangle _card;
     private int _atk, _row;
 
     public Card(int atk, int row){
+	super(atk, row);
 	this.setSize(50,50);
+	this.setFillColor(java.awt.Color.BLACK);
 	_atk = atk;
 	_row = row;	
     }
@@ -19,4 +22,3 @@ public class Card extends Rectangle{
     }
 
 }
-
