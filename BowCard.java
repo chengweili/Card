c71a88e24462abc65f98dbe3b63532aef6961b24
chengweili <chengweili42@gmail.com>
2016-01-22 08:39:00 -0500

    Update BowCard.java

--- BowCard.java
+++ BowCard.java
@@ -1,12 +1,9 @@
 import wheels.users.*;
 
 public class SwordCard{
-   
-    private int _row;
 
-    public SwordCard(int atk){
-	super(atk);
-	_row = 1;
+    public SwordCard(int atk, int row){
+	super(atk,row);
     }
 
 }
